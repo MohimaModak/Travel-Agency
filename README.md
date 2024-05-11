499afdaefd4a69aec8d2d4b2a82c7ca188ef3515
mohima13 <modakmohima46@gmail.com>
2024-05-11 23:36:42 +0600

    uploaded the READMe file

---
+++ README.md
@@ -0,0 +1,25 @@
+# **Travel Agency**
+
+Live link: swift-bit.surge.sh
+
+-You can go through by clicking the live link given below. Also, if you want to run it on your local device, you can use `npm run dev` to run this project.
+
+-I have spend one day completing this project.
+
+-I have completed this type of project for the first time, so this is a new experience for me. Additionally, I have implemented some new designs.
+
+
+-1st project: 
+Client Site : https://github.com/programming-hero-web-course1/b8a12-client-side-MohimaModak
+ Server Site: https://github.com/programming-hero-web-course1/b8a12-server-side-MohimaModak
+ Live link: https://final-project-a1a7c.web.app/
+
+
+ -2nd project:
+ Client Site: https://github.com/programming-hero-web-course-4/b8a10-brandshop-client-side-MohimaModak
+Server Site: https://github.com/programming-hero-web-course-4/b8a10-brandshop-server-side-MohimaModak
+Live link: https://assignment-ten-project-85a1e.web.app/
+
+
+
+
--- src/assets/Component/Travel/Travel.jsx
+++ src/assets/Component/Travel/Travel.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 import AOS from "aos";
 import "./Travel.css";
 const Travel = () => {
@@ -18,50 +18,73 @@ const Travel = () => {
   return (
     <div className="vacationFont ">
       <div className="font p-2 sm:p-8 md:p-16">
-        <h1 data-aos="zoom-in" className="text-2xl sm:text-3xl md:text-4xl text-center  pb-10 vacationHeading">
+        <h1
+          data-aos="zoom-in"
+          className="text-2xl sm:text-3xl md:text-4xl text-center  pb-10 vacationHeading"
+        >
           About <span className="text-blue-400">Destinations</span>{" "}
         </h1>
-        <img
-          src="https://icons8.com/icon/qkWln6qR0wgr/around-the-globe"
-          alt=""
-          srcset=""
-        />
+
         <div
-          className="flex justify-center items-center text-center vacationFont  rounded-lg overflow-x-auto overflow-y-auto"
+          className="flex justify-center items-center text-center border vacationFont  rounded-lg overflow-scroll"
           style={{ overflowX: "auto" }}
         >
           <table className="table-auto">
             <thead className="">
               <tr className="px-16 py-4 rounded-md shadow-sm border">
-                <th data-aos="zoom-in" className="px-16 text-xl py-4 rounded-md shadow-md border text-blue-400 vacationHeading">
+                <th
+                  data-aos="zoom-in"
+                  className="px-16 text-xl py-4 rounded-md shadow-md border text-blue-400 vacationHeading"
+                >
                   Class
                 </th>
-              
-                <th data-aos="zoom-in" className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading">
+
+                <th
+                  data-aos="zoom-in"
+                  className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading"
+                >
                   Fare Basis
                 </th>
-                <th data-aos="zoom-in" className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading">
+                <th
+                  data-aos="zoom-in"
+                  className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading"
+                >
                   Itinerary
                 </th>
-                <th data-aos="zoom-in" className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading">
+                <th
+                  data-aos="zoom-in"
+                  className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading"
+                >
                   Seat
                 </th>
-                <th data-aos="zoom-in" className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading">
+                <th
+                  data-aos="zoom-in"
+                  className="px-5 md:px-16 py-4 text-xl rounded-md shadow-md border text-blue-400 vacationHeading"
+                >
                   Price
                 </th>
               </tr>
             </thead>
             <tbody>
               {TravelInfo.map((offer, index) => (
-                <tr data-aos="zoom-in" key={index} >
-                  <td data-aos="zoom-in" className=" px-5 md:px-16 py-4 rounded-lg  shadow-md  border font-semibold">
+                <tr data-aos="zoom-in" key={index}>
+                  <td
+                    data-aos="zoom-in"
+                    className=" px-5 md:px-16 py-4 rounded-lg  shadow-md  border font-semibold"
+                  >
                     {offer.class}
                   </td>
-                 
-                  <td data-aos="zoom-in" className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold">
+
+                  <td
+                    data-aos="zoom-in"
+                    className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold"
+                  >
                     {offer.fareBasis}
                   </td>
-                  <td data-aos="zoom-in" className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold">
+                  <td
+                    data-aos="zoom-in"
+                    className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold"
+                  >
                     <ul>
                       {offer.itineraries.map((itinerary, i) => (
                         <li key={i}>
@@ -71,13 +94,21 @@ const Travel = () => {
                       ))}
                     </ul>
                   </td>
-                  <td data-aos="zoom-in" className="px-16py-4 rounded-lg shadow-md  border font-semibold">
+                  <td
+                    data-aos="zoom-in"
+                    className="px-16py-4 rounded-lg shadow-md  border font-semibold"
+                  >
                     {offer.seat}
                   </td>
-                  <td data-aos="zoom-in" className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold">
+                  <td
+                    data-aos="zoom-in"
+                    className=" px-5 md:px-16 py-4 rounded-lg shadow-md  border font-semibold"
+                  >
                     {offer.price}
                     <br />
-                    <button className="bg-blue-400 text-white my-2 px-5 py-1.5 rounded-full">SELECT</button>
+                    <button className="bg-blue-400 text-white my-2 px-5 py-1.5 rounded-full">
+                      SELECT
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -85,7 +116,6 @@ const Travel = () => {
           </table>
         </div>
       </div>
-      
     </div>
   );
 };
